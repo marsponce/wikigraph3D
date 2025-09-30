a19dd28b94f26759ea9573bc4154ae3abbfd879f
Mars Ponce <marcelo.ponce.ardon@gmail.com>
2025-09-30 11:42:36 -0400

    Rename DynamicGraph3dBtched.tsx -> Graph.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,6 @@
 // src/app/components/Main.tsx
 "use client";
-import Graph from "./DynamicGraph3DBatched";
+import Graph from "./Graph";
 import Sidebar from "./Sidebar";
 import { useState } from "react";
 
@@ -9,10 +9,7 @@ export default function Main() {
   return (
     <div className="relative">
       <Graph onNodeSelect={setSelectedNode} className="" />
-      <Sidebar
-        node={selectedNode}
-        className="absolute top-0 right-0 w-80  bg-transparent shadow-lg z-50 overflow-y-auto overflow-x-hidden"
-      />
+      <Sidebar node={selectedNode} className="" />
     </div>
   );
 }

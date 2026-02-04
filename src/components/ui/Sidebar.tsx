f5e83934b3a14d0fc4890aebeb7a779b74aa32a5
Mars Ponce <mars@seahorse.fishhaus.net>
2026-02-04 18:15:52 -0500

    Removed mobile specific styling, decided the difference didn't make sense and it would be fine anyways. Fixed animation slowdown issue when hiding sidebar by using translation

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -64,26 +64,27 @@ export default function Sidebar({
       <aside
         className={clsx(
           // Base styles
-          "backdrop-blur-sm sm:backdrop-blur-md md:backdrop-blur-lg lg:backdrop-blur-xl",
-          "fixed right-0 z-3",
-          "flex flex-col sm:flex-row",
-          "w-screen sm:h-screen",
-          "transition-transform duration-500",
+          "backdrop-blur-lg",
+          "fixed right-0 top-0 z-3",
+          "flex flex-row",
+          "h-screen",
+          "transition-all duration-500",
+          "w-screen",
           // State-specific styles
           {
-            "bg-white/10 h-16 sm:h-full sm:w-13": sidebarState === "closed",
-            "rounded-none w-screen bg-black/30": sidebarState === "fullscreen",
-            "sm:bg-white/10 h-auto sm:w-1/3": sidebarState === "open",
+            "bg-white/10 [transform:translateX(calc(100%-4rem))]":
+              sidebarState === "closed",
+            "sm:bg-black/60": sidebarState === "open",
           },
         )}
         data-sidebar-state={sidebarState}
       >
-        {/* sidebar button */}
+        {/* sidebar buttons */}
         <div
           className={clsx(
-            "flex flex-row sm:flex-col",
+            "flex flex-col",
             "place-content-center place-items-center",
-            "space-x-2 sm:space-y-2 sm:space-x-0",
+            "space-y-2 space-x-0",
             "w-fit p-3",
           )}
         >
@@ -95,23 +96,6 @@ export default function Sidebar({
             }
           >
             <InformationCircleIcon />
-          </Button>
-          {/* full screen button */}
-          <Button
-            onClick={toggleFullscreen}
-            className="hidden sm:block" // show only on non-mobile screens
-            toggled={sidebarState === "fullscreen"}
-            aria-label={
-              sidebarState === "fullscreen"
-                ? "Minimize Sidebar"
-                : "Maximize Sidebar"
-            }
-          >
-            {sidebarState === "fullscreen" ? (
-              <ArrowsPointingInIcon />
-            ) : (
-              <ArrowsPointingOutIcon />
-            )}
           </Button>
           {/* focus button */}
           <Button
@@ -129,6 +113,15 @@ export default function Sidebar({
             "overflow-y-auto overflow-x-hidden",
             "p-3",
             "relative",
+            "[transform:translateZ(0)]",
+            "[will-change:auto]",
+            "transition-all",
+            {
+              "": sidebarState === "closed",
+              // "block translate-x-0": sidebarState === "fullscreen",
+              "": sidebarState === "open",
+              //"max-w-screen": sidebarState === "open",
+            },
           )}
           data-sidebar-state={sidebarState}
         >
@@ -143,7 +136,6 @@ export default function Sidebar({
             selectedNode={selectedNode}
             setSelectedNode={setSelectedNode}
             setGraphData={setGraphData}
-            sidebarState={sidebarState}
           />
           <BreadCrumbs
             graphData={graphData}

a0b98d2330f1b0a1e27cec0b7368f62b15382946
Mars Ponce <mars@marsponce.ca>
2026-02-25 14:07:25 -0500

    Implement download toast

--- src/components/ui/DownloadCard.tsx
+++ src/components/ui/DownloadCard.tsx
@@ -12,20 +12,44 @@ export default function DownloadCard({
   graphRef,
   graphData,
 }: DownloadCardProps) {
+  const download = (type: "json" | "glb") => {
+    setTimeout(() => {
+      toast.promise(
+        (async () => {
+          // throw new Error("test error"); // for testing
+          if (type === "json") downloadGraphJSON(graphData);
+          else downloadGraphModel(graphRef);
+        })(),
+        {
+          loading: "Downloading...",
+          success: "Downloaded successfully",
+          error: () => ({
+            message: "Download Failed",
+            duration: 5000,
+            action: {
+              label: "Retry",
+              onClick: () => download(type),
+            },
+          }),
+        },
+      );
+    }, 50);
+  };
+
   return (
     <>
       <div className="flex flex-col justify-center h-full w-fit mx-auto">
         <button
           type="button"
           className="flex-none m-3 p-3 text-lg pointer-events-auto rounded transition-colors duration-300 bg-gray-900 hover:bg-sky-600 active:bg-sky-100 text-white"
-          onClick={() => downloadGraphJSON(graphData)}
+          onClick={() => download("json")}
         >
           Download as JSON
         </button>
         <button
           type="button"
           className="flex-none m-3 p-3 text-lg pointer-events-auto rounded transition-colors duration-300 bg-gray-900 hover:bg-sky-600 active:bg-sky-100 text-white"
-          onClick={() => downloadGraphModel(graphRef)}
+          onClick={() => download("glb")}
         >
           Download as GLB
         </button>

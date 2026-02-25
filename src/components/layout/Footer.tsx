33c200f1616e020929b1b80c2182c04f62b95ad0
Mars Ponce <mars@marsponce.ca>
2026-02-25 14:17:54 -0500

    Remove FooterProps since it's not used

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,8 +1,4 @@
 // src/app/components/Footer.tsx
-type FooterProps = {
-  children?: React.ReactNode;
-};
-
 export default function Footer() {
   return (
     <footer className="absolute bottom-0 left-0 w-full pointer-events-none">

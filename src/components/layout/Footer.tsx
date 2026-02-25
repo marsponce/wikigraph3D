7716d7e5c4a4aeeeb66a1bd281d5c177d6c9a51d
Mars Ponce <mars@marsponce.ca>
2026-02-25 16:56:52 -0500

    chore: install commitlint, source version & name in Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,11 +1,12 @@
 // src/app/components/Footer.tsx
+import { version, name } from "../../../package.json";
+
 export default function Footer() {
   return (
     <footer className="absolute bottom-0 left-0 w-full pointer-events-none">
       <h6 className="relative">
         <span className="absolute left-4 bottom-4">
-          {process.env.NEXT_PUBLIC_APP_NAME}{" "}
-          {process.env.NEXT_PUBLIC_APP_VERSION} 2026
+          {name} {version}
         </span>
       </h6>
     </footer>
